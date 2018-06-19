9bfb58506d5d019520d0b784f97ad216bf6bca09
gudanrenao <zhangwenit@126.com>
2018-06-19 10:51:54 +0800

    冒泡排序及优化

--- src/main/java/com/zhangwen/learn/zhangwenit/algorithm/sorting_basic/util/SortTestHelper.java
+++ src/main/java/com/zhangwen/learn/zhangwenit/algorithm/sorting_basic/util/SortTestHelper.java
@@ -31,15 +31,16 @@ public class SortTestHelper {
     // swapTimes定义了数组的无序程度:
     // swapTimes == 0 时, 数组完全有序
     // swapTimes 越大, 数组越趋向于无序
-    public static Integer[] generateNearlyOrderedArray(int n, int swapTimes){
+    public static Integer[] generateNearlyOrderedArray(int n, int swapTimes) {
 
         Integer[] arr = new Integer[n];
-        for( int i = 0 ; i < n ; i ++ )
-            arr[i] = new Integer(i);
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
 
-        for( int i = 0 ; i < swapTimes ; i ++ ){
-            int a = (int)(Math.random() * n);
-            int b = (int)(Math.random() * n);
+        for (int i = 0; i < swapTimes; i++) {
+            int a = (int) (Math.random() * n);
+            int b = (int) (Math.random() * n);
             int t = arr[a];
             arr[a] = arr[b];
             arr[b] = t;
