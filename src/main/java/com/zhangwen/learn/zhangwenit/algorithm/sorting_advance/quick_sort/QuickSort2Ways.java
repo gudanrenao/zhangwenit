0329597ee85748f371a2a9e4398874cd95e7b511
gudanrenao <zhangwenit@126.com>
2018-06-22 19:24:19 +0800

    快速排序：双路快速排序&三路快速排序

--- src/main/java/com/zhangwen/learn/zhangwenit/algorithm/sorting_advance/quick_sort/QuickSort2Ways.java
+++ src/main/java/com/zhangwen/learn/zhangwenit/algorithm/sorting_advance/quick_sort/QuickSort2Ways.java
@@ -1,16 +1,15 @@
 package com.zhangwen.learn.zhangwenit.algorithm.sorting_advance.quick_sort;
 
-import com.zhangwen.learn.zhangwenit.algorithm.sorting_basic.insertion_sort.InsertionSort;
 import com.zhangwen.learn.zhangwenit.algorithm.sorting_basic.util.SortTestHelper;
 
 import java.util.Arrays;
 
 /**
- * 快速排序
+ * 双路快速排序(对近乎有序数组效率高)
  *
- * @author zhangwen at 2018-06-20 12:09
+ * @author zhangwen at 2018-06-21 12:09
  **/
-public class QuickSort {
+public class QuickSort2Ways {
 
     public static void sort(Comparable[] arr) {
         quickSort(arr, 0, arr.length - 1);
@@ -28,12 +27,6 @@ public class QuickSort {
         if (l >= r) {
             return;
         }
-        //优化1：高级排序在递归到只包含少量元素的时候都可以使用插入排序进行优化
-//        if (r - l <= 15) {
-//            InsertionSort.insertSort(arr, l, r);
-//            return;
-//        }
-
         //获取快速排序后partition操作后的p的索引
         int p = partition(arr, l, r);
         quickSort(arr, l, p - 1);
@@ -41,6 +34,7 @@ public class QuickSort {
     }
 
     /**
+     * 个人优化方案
      * 快速排序操作,使得arr[l...p-1]<arr[p],arr[p+1...r] > arr[p]
      *
      * @param arr
@@ -48,26 +42,26 @@ public class QuickSort {
      * @param r
      * @return p
      */
-    private static int partition(Comparable[] arr, int l, int r) {
-        //优化点2：对于一个近乎有序的数组，如果选择第一个元素作为比较point
-        //那么这个算法将会退化为o[n方]的时间复杂度
-        //为了解决这个问题，需要在数据中随机选择一个元素作为point,并将其与第一个元素交换
+    private static int partition2(Comparable[] arr, int l, int r) {
+        //优化点2：数据中随机选择一个元素作为point,并将其与第一个元素交换
         SortTestHelper.swap(arr, l, (int) (Math.random() * (r - l + 1) + l));
         //已第一个值为比较值
         Comparable e = arr[l];
-        //比较点最后所在索引,关键：同时也是比较过程中比 e 小的数组的最后一个元素索引!!!
-        //因为在全部比较完之后，会把 e 与 该元素交换位置
+        //比较点最后所在索引
         int p = l;
         //根据定义，需使得arr[l + 1...p] < e,arr[p+1...i) < e
         // 注意：后面的数据是前闭后开区间！！！，因为当前i元素是将要参与比较的下一个元素
+        int choice = 1;
         for (int i = p + 1; i <= r; i++) {
             //如果该值大于e,则无需任何操作
             //如果小于e,则需要把arr[p+1...i)的e[p+1]和arr[i]进行交换，并且p后移一位！！！
             if (arr[i].compareTo(e) < 0) {
-                SortTestHelper.swap(arr, i, p + 1);
-                p++;
-                //上面两行可以合并改写为下面这条语句
-                // SortTestHelper.swap(arr, i, ++p);
+                SortTestHelper.swap(arr, i, ++p);
+            } else if (arr[i].compareTo(e) == 0) {
+                choice = -choice;
+                if (choice == 1) {
+                    SortTestHelper.swap(arr, i, ++p);
+                }
             }
         }
         //arr[l]与 arr[p]交换
@@ -75,13 +69,45 @@ public class QuickSort {
         return p;
     }
 
+    /**
+     * 双路快速排序法
+     *
+     * @param arr
+     * @param l
+     * @param r
+     * @return
+     */
+    private static int partition(Comparable[] arr, int l, int r) {
+
+        SortTestHelper.swap(arr, l, (int) (Math.random() * (r - l + 1) + l));
+        Comparable e = arr[l];
+        //arr[l+1...i)< e , arr(j...r] > e
+        int i = l + 1, j = r;
+        while (true) {
+            while (i <= r && arr[i].compareTo(e) < 0) {
+                i++;
+            }
+            while (j >= l + 1 && arr[j].compareTo(e) > 0) {
+                j--;
+            }
+            if (i > j) {
+                break;
+            }
+            SortTestHelper.swap(arr, i, j);
+            i++;
+            j--;
+        }
+        SortTestHelper.swap(arr, l, j);
+        return j;
+    }
+
     public static void main(String[] args) {
-        int n = 100000;
-        Integer[] arr = SortTestHelper.generateRandomArray(n, 0, 10);
-//        Integer[] arr = SortTestHelper.generateNearlyOrderedArray(1000000, 0);
+        int n = 1000000;
+//        Integer[] arr = SortTestHelper.generateRandomArray(n, 0, 1000000);
+        Integer[] arr = SortTestHelper.generateNearlyOrderedArray(1000000, 0);
         Integer[] arr2 = Arrays.copyOf(arr, arr.length);
         SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
-                "sorting_advance.quick_sort.QuickSort", arr);
+                "sorting_advance.quick_sort.QuickSort2Ways", arr);
         SortTestHelper.printArray(arr, 10000);
         SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
                 "sorting_advance.merge_sort.MergeSort", arr2);
---
+++ src/main/java/com/zhangwen/learn/zhangwenit/algorithm/sorting_advance/quick_sort/QuickSort3Ways.java
@@ -0,0 +1,92 @@
+package com.zhangwen.learn.zhangwenit.algorithm.sorting_advance.quick_sort;
+
+import com.zhangwen.learn.zhangwenit.algorithm.sorting_basic.util.SortTestHelper;
+
+import java.util.Arrays;
+
+/**
+ * 三路快速排序(对有大量重复key的数据效率高)
+ *
+ * @author zhangwen at 2018-06-21 18:00
+ **/
+public class QuickSort3Ways {
+
+    public static void sort(Comparable[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    /**
+     * 递归快速排序
+     *
+     * @param arr
+     * @param l
+     * @param r
+     */
+    private static void quickSort(Comparable[] arr, int l, int r) {
+        //递归终止条件，当l >= r
+        if (l >= r) {
+            return;
+        }
+        //partition
+        SortTestHelper.swap(arr, l, (int) (Math.random() * (r - l + 1) + l));
+        Comparable e = arr[l];
+        //arr[l+1...lt] < e , arr[lt+1...i) = e , arr[gt,r] > e
+        //初始化关键索引位置
+        int lt = l, gt = r + 1, i = l + 1;
+        while (i < gt) {
+            if (arr[i].compareTo(e) < 0) {
+                //交换
+                SortTestHelper.swap(arr, i, lt + 1);
+                lt++;
+                i++;
+            } else if (arr[i].compareTo(e) > 0) {
+                SortTestHelper.swap(arr, i, gt - 1);
+                gt--;
+            } else {
+                i++;
+            }
+        }
+        SortTestHelper.swap(arr, l, lt);
+
+        //递归
+        quickSort(arr, l, lt - 1);
+        quickSort(arr, gt, r);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("普通数组排序结果：");
+        int n = 1000000;
+        Integer[] arr1 = SortTestHelper.generateRandomArray(n, 0, 1000000);
+        Integer[] arr2 = Arrays.copyOf(arr1, arr1.length);
+        Integer[] arr3 = Arrays.copyOf(arr1, arr1.length);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.merge_sort.MergeSort", arr1);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.quick_sort.QuickSort2Ways", arr2);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.quick_sort.QuickSort3Ways", arr3);
+
+        System.out.println("近乎有序数组排序结果：");
+        arr1 = SortTestHelper.generateNearlyOrderedArray(n, 10);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.merge_sort.MergeSort", arr1);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.quick_sort.QuickSort2Ways", arr2);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.quick_sort.QuickSort3Ways", arr3);
+        SortTestHelper.printArray(arr2,10000);
+
+        System.out.println("大量重复数组排序结果：");
+        arr1 = SortTestHelper.generateRandomArray(n, 0, 100);
+        arr2 = Arrays.copyOf(arr1, arr1.length);
+        arr3 = Arrays.copyOf(arr1, arr1.length);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.merge_sort.MergeSort", arr1);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.quick_sort.QuickSort2Ways", arr2);
+        SortTestHelper.testSort("com.zhangwen.learn.zhangwenit.algorithm." +
+                "sorting_advance.quick_sort.QuickSort3Ways", arr3);
+    }
+}
