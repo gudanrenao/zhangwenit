4b7e80d89c7ef3430140053bf7c2bea4c7ea0423
gudanrenao <zhangwenit@126.com>
2019-05-29 17:32:59 +0800

    根据swagger生成confluence文档2

--- src/main/java/com/zhangwen/learn/zhangwenit/swagger/SwaggerToConfluenceDemo.java
+++ src/main/java/com/zhangwen/learn/zhangwenit/swagger/SwaggerToConfluenceDemo.java
@@ -1,9 +1,9 @@
 package com.zhangwen.learn.zhangwenit.swagger;
 
-import io.github.swagger2markup.Swagger2MarkupConfig;
-import io.github.swagger2markup.Swagger2MarkupConverter;
-import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
-import io.github.swagger2markup.markup.builder.MarkupLanguage;
+//import io.github.swagger2markup.Swagger2MarkupConfig;
+//import io.github.swagger2markup.Swagger2MarkupConverter;
+//import io.github.swagger2markup.builder.Swagger2MarkupConfigBuilder;
+//import io.github.swagger2markup.markup.builder.MarkupLanguage;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,19 +18,19 @@ import java.nio.file.Paths;
 public class SwaggerToConfluenceDemo {
 
     /**
-     * 根据swagger生成confluence文档
+     * 根据swagger生成confluence文档，需要放开maven.xml 中swagger2markup
      *
      * @throws MalformedURLException
      */
     public static void main(String[] args) throws MalformedURLException {
-        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
-                .withMarkupLanguage(MarkupLanguage.CONFLUENCE_MARKUP)
-                .build();
-
-        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
-                .withConfig(config)
-                .build()
-                .toFolder(Paths.get("src/docs/confluence/generated"));
-//                .toFile(Paths.get("src/docs/confluence/generated/all")); //整合到一个文件中
+//        Swagger2MarkupConfig config = new Swagger2MarkupConfigBuilder()
+//                .withMarkupLanguage(MarkupLanguage.CONFLUENCE_MARKUP)
+//                .build();
+//
+//        Swagger2MarkupConverter.from(new URL("http://localhost:8080/v2/api-docs"))
+//                .withConfig(config)
+//                .build()
+//                .toFolder(Paths.get("src/docs/confluence/generated"));
+////                .toFile(Paths.get("src/docs/confluence/generated/all")); //整合到一个文件中
     }
 }
